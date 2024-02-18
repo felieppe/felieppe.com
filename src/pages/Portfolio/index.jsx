14f1f81426bc1eba86f09fa7addb848d37435327
Felipe Cabrera <me@felieppe.com>
2024-02-18 03:40:29 -0300

    Added projects sorting by selecting existing tags

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
+
 import './portfolio.css'
 
 import Header from '../../components/Header'
 import Project from './project'
-
 
 import Felieppe from '../../assets/felieppe.com_cover.png'
 import Naval from '../../assets/naval_cover.png'
@@ -10,6 +11,21 @@ import Minepy from '../../assets/minepy_cover.png'
 import Conways from '../../assets/conways_cover.png'
 
 function Portfolio() {
+    const [ selectedTag, setSelectedTag ] = useState('ALL')
+
+    const projects = [
+        { imageSrc: Felieppe, imageBoxColor: "#2d2d2d", infoBoxColor: "#cdcdcd", tags: ["WEBSITE", "REACT.JS", "FRONTEND"], gotoLink: "https://felieppe.com", githubUrl: "https://github.com/felieppe/felieppe.com", name: "Felieppe.com" },
+        { imageSrc: Naval, imageBoxColor: "#1e3c72", infoBoxColor: "#3c6fc9", tags: ["MOBILE", "TELEGRAM", "BOTCHAT"], githubUrl: "https://github.com/felieppe/NavalBattle", name: "Naval Battle" },
+        { imageSrc: Minepy, imageBoxColor: "#b93d46", infoBoxColor: "#d95f67", tags: ["CONSOLE", "PYTHON", "GAME"], githubUrl: "https://github.com/felieppe/Minepy", name: "Minepy" },
+        { imageSrc: Conways, imageBoxColor: "#18a86b", infoBoxColor: "#2fb383", tags: ["CONSOLE", ".NET", "GAME"], githubUrl: "https://github.com/felieppe/ConwaysLife", name: "Conways Life" }
+    ]
+
+    const handleTagClick = (tag) => {
+        setSelectedTag(tag)
+    }
+
+    const filteredProjects = selectedTag === 'ALL' ? projects : projects.filter(project => project.tags.includes(selectedTag));
+
     return (
         <>
             <Header />
@@ -21,19 +37,20 @@ function Portfolio() {
 
                 <div className='portfolio-tags-selector'>
                     <ul>
-                        <li><button className='portfolio-tag-active'>ALL</button></li>
-                        <li><button>DESKTOP</button></li>
-                        <li><button>MOBILE</button></li>
-                        <li><button>CONSOLE</button></li>
-                        <li><button>WEBSITE</button></li>
+                        <li><button className={selectedTag === 'ALL' ? 'portfolio-tag-active' : ''} onClick={() => handleTagClick('ALL')}>ALL</button></li>
+                        <li><button className={selectedTag === 'DESKTOP' ? 'portfolio-tag-active' : ''} onClick={() => handleTagClick('DESKTOP')}>DESKTOP</button></li>
+                        <li><button className={selectedTag === 'MOBILE' ? 'portfolio-tag-active' : ''} onClick={() => handleTagClick('MOBILE')}>MOBILE</button></li>
+                        <li><button className={selectedTag === 'CONSOLE' ? 'portfolio-tag-active' : ''} onClick={() => handleTagClick('CONSOLE')}>CONSOLE</button></li>
+                        <li><button className={selectedTag === 'WEBSITE' ? 'portfolio-tag-active' : ''} onClick={() => handleTagClick('WEBSITE')}>WEBSITE</button></li>
                     </ul>
                 </div>
 
                 <div className='portfolio-content'>
-                    <Project imageSrc={Felieppe} imageBoxColor={"#2d2d2d"} infoBoxColor={"#cdcdcd"} tags={["WEBSITE", "REACT.JS", "FRONTEND"]} gotoLink={"https://felieppe.com"} githubUrl={"https://github.com/felieppe/felieppe.com"} name={"Felieppe.com"} />
-                    <Project imageSrc={Naval} imageBoxColor={"#1e3c72"} infoBoxColor={"#3c6fc9"} tags={["MOBILE", "TELEGRAM", "BOTCHAT"]} githubUrl={"https://github.com/felieppe/NavalBattle"} name={"Naval Battle"} />
-                    <Project imageSrc={Minepy} imageBoxColor={"#b93d46"} infoBoxColor={"#d95f67"} tags={["CONSOLE", "PYTHON", "GAME"]} githubUrl={"https://github.com/felieppe/Minepy"} name={"Minepy"} />
-                    <Project imageSrc={Conways} imageBoxColor={"#18a86b"} infoBoxColor={"#2fb383"} tags={["CONSOLE", ".NET", "GAME"]}  githubUrl={"https://github.com/felieppe/ConwaysLife"} name={"Conways Life"} />
+                    {filteredProjects.map((project, index) => {
+                        return (
+                            <Project key={index} {...project} />
+                        )
+                    })}
                 </div>       
             </div>
         </>
