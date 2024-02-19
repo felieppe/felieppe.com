576f673f10f975270b66b7ac7271847a1ba528fd
Felipe Cabrera <me@felieppe.com>
2024-02-19 16:47:39 -0300

    Added Privacy policy page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={ <Home /> } />
         <Route path="/portfolio" element={ <Portfolio /> } />
         <Route path="/contact" element={ <Contact /> } />
+
+        <Route path="/privacy" element={ <Privacy /> } />
       </Routes>
     </BrowserRouter>
   )
