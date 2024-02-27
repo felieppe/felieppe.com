2946c5c0a2f071275438bf8c8e0ed12c99d63add
Felipe Cabrera <me@felieppe.com>
2024-02-27 16:29:50 -0300

    Added Autoshar project to portfolio

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -13,6 +13,7 @@ import Naval from '../../assets/naval_cover.png'
 import Minepy from '../../assets/minepy_cover.png'
 import Conways from '../../assets/conways_cover.png'
 import Challenge from '../../assets/challenge_cover.png'
+import Autosharp from '../../assets/autosharp_cover.png'
 
 function Portfolio() {
     useEffect(() => {
@@ -26,7 +27,8 @@ function Portfolio() {
         { imageSrc: Naval, imageBoxColor: "#1e3c72", infoBoxColor: "#3c6fc9", tags: ["MOBILE", "TELEGRAM", "BOTCHAT"], githubUrl: "https://github.com/felieppe/NavalBattle", name: "Naval Battle" },
         { imageSrc: Minepy, imageBoxColor: "#b93d46", infoBoxColor: "#d95f67", tags: ["CONSOLE", "PYTHON", "GAME"], githubUrl: "https://github.com/felieppe/Minepy", name: "Minepy" },
         { imageSrc: Conways, imageBoxColor: "#18a86b", infoBoxColor: "#2fb383", tags: ["CONSOLE", ".NET", "GAME"], githubUrl: "https://github.com/felieppe/ConwaysLife", name: "Conways Life" },
-        { imageSrc: Challenge, imageBoxColor: "#052d66", infoBoxColor: "#e3e3e3", tags: ["MOBILE", "MOCKUP", "ECOLOGICAL"], ytUrl: "https://www.youtube.com/watch?v=7di4GbpxrWM", name: "Desafío Ingeniería" }
+        { imageSrc: Challenge, imageBoxColor: "#052d66", infoBoxColor: "#e3e3e3", tags: ["MOBILE", "MOCKUP", "ECOLOGICAL"], ytUrl: "https://www.youtube.com/watch?v=7di4GbpxrWM", name: "Desafío Ingeniería" },
+        { imageSrc: Autosharp, imageBoxColor: "#055d66", infoBoxColor: "#e3e3e3", tags: ["DESKTOP", ".NET", "AUTOMATION"], githubUrl: "https://github.com/felieppe/Autosharp", name: "Autosharp" }
     ]
 
     const handleTagClick = (tag) => {
